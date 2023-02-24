da90bf76b0bee4d36bdc3d38ee4ea0829de2a725
pasan nissanka <pasannissanka@hotmail.com>
2023-02-24 14:53:39 +0530

    old: Fixed a bug + update application.properties

--- medicine_finder_backend/src/main/java/com/medifinder/medifinder/repositories/PharmaRepository.java
+++ medicine_finder_backend/src/main/java/com/medifinder/medifinder/repositories/PharmaRepository.java
@@ -4,10 +4,12 @@ import com.medifinder.medifinder.entities.Pharma;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface PharmaRepository extends JpaRepository<Pharma, String> {
     @Query("select p from Pharma p where p.user.email = ?1")
     Optional<Pharma> findByUser_Email(String email);
