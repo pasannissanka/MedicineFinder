7df23a1d43a734c8d8f907e83b8528dd1c648003
pasan nissanka <pasannissanka@hotmail.com>
2023-02-28 14:05:54 +0530

    init oauth server

---
+++ web_services/medifinder.cloud.oauth.service/src/test/java/com/pasan/medifinder/cloud/oauth/service/MedifinderOAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pasan.medifinder.cloud.oauth.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedifinderOAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
